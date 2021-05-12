2b5c4db150f61154065dfcfcf3653b902f2733cc
Tibor Répási <rtib@users.noreply.github.com>
2021-05-12 09:32:40 +0200

    spec

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -13,7 +13,7 @@ RSpec.configure do |config|
     Facter.clear_messages
 
     RSpec::Mocks.setup
-    
+
     if ENV['PUPPET_DEBUG']
       Puppet::Util::Log.level = :debug
       Puppet::Util::Log.newdestination(:console)
